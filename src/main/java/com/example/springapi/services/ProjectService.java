332acb5556a6ad6089ce5335df4eea9c0daabc5d
Alexander Ortiz <CL198785156@enel.com>
2022-10-21 18:31:08 -0300

    Add crud

--- src/main/java/com/example/springapi/services/ProjectService.java
+++ src/main/java/com/example/springapi/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.example.springapi.services;
 
 import com.example.springapi.domain.Project;
+import com.example.springapi.exceptions.ProjectIdException;
 import com.example.springapi.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +11,42 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+    public Project saveProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception ex) {
+            throw new ProjectIdException("Producto con Id: " + project.getProjectIdentifier().toUpperCase() + " ya existe.");
+        }
     }
 
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Producto " + projectId + " no existe");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("No se pudo encontrar proyecto con id " + projectId);
+        }
+        projectRepository.delete(project);
+    }
+
+    public Project updateProject(Project project) {
+        project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+        Project oldProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+        if (oldProject == null) {
+            throw new ProjectIdException("No se pudo modificar proyecto con id: " + project.getProjectIdentifier());
+        }
+        project.setId(oldProject.getId());
+        return projectRepository.save(project);
+    }
 }
